d3ef0001bd62c41af39bec31436968683b68fa7a
thinkyside <eunlyuing@gmail.com>
2024-07-15 22:33:18 +0900

    [MVP_UIKit] Xcode 프로젝트 생성

---
+++ NumbersAPI_MVP_UIKit/NumbersAPI_MVP_UIKit/NumberViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumbersAPI_MVP_UIKit
+//
+//  Created by 김민준 on 7/15/24.
+//
+
+import UIKit
+
+class NumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
