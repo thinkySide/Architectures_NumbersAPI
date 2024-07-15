185e85cdc2b28e69a1c70861e1f69ce29d1d5a37
thinkyside <eunlyuing@gmail.com>
2024-07-15 20:45:21 +0900

    [MVC_UIKit] Xcode 파일 생성

---
+++ NumbersAPI_MVC_UIKit/NumbersAPI_MVC_UIKit/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NumbersAPI_MVC_UIKit
+//
+//  Created by 김민준 on 7/15/24.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ NumbersAPI_MVC_UIKit/NumbersAPI_MVC_UIKit/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  NumbersAPI_MVC_UIKit
+//
+//  Created by 김민준 on 7/15/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = HomeViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
